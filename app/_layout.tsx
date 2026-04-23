46b3d9103a51128467d180dd004ac31fc727ac12
aliixh <aliixhuang@gmail.com>
2026-04-23 15:19:04 -0700

    landscape p2

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,19 @@
-import { Stack } from 'expo-router';
-import { useFonts, PixelifySans_400Regular, PixelifySans_700Bold } from '@expo-google-fonts/pixelify-sans';
-import { NovaCut_400Regular } from '@expo-google-fonts/nova-cut';
 import { Gluten_400Regular, Gluten_700Bold } from '@expo-google-fonts/gluten';
-
-export const unstable_settings = {
-  initialRouteName: 'onboarding',
-};
+import { NovaCut_400Regular } from '@expo-google-fonts/nova-cut';
+import { PixelifySans_400Regular, PixelifySans_700Bold, useFonts } from '@expo-google-fonts/pixelify-sans';
+import { Stack } from 'expo-router';
+import * as ScreenOrientation from 'expo-screen-orientation';
+import { useEffect } from 'react';
 
 export default function RootLayout() {
+
+  useEffect(() => {
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+    return () => {
+      ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.DEFAULT);
+    };
+  }, []);
+  
   const [fontsLoaded] = useFonts({
     PixelifySans_400Regular,
     PixelifySans_700Bold,
